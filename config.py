050347e6a5f9d5b3198d139a797c3a1140808ad0
Chiru5172 <g.chirudeepreddy@gmail.com>
2025-12-27 20:59:07 +0530

    Prepared project for deployment

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Secret key for session management
-app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.secret_key = os.environ.get("SECRET_KEY", "dev-secret")
 
-# MySQL Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Mysql@localhost/ecom_db'
+DB_USER = os.environ.get("root")
+DB_PASSWORD = os.environ.get("asblrbgMenWEcOrRblZhObKjgQwdvbNl")
+DB_HOST = os.environ.get("mysql.railway.internal")
+DB_NAME = os.environ.get("railway")
+DB_PORT = os.environ.get("DB_PORT", "3306")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql://root:asblrbgMenWEcOrRblZhObKjgQwdvbNl@nozomi.proxy.rlwy.net:23123/railway"
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialize database
 db = SQLAlchemy(app)
-
-import os
-
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
